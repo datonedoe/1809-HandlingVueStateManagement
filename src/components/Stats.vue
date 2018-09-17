d4dea6a34731de8f47df65faaa1101c84ac427ce
Darwin2 <Darwin1@Darwins-MBP-1.localdomain>
2018-09-17 16:49:45 -0700

    finished tutorial - Learn Vuex By Example, Handling Vue State Management

---
+++ src/components/Stats.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="stats">
+    <h1>A different component</h1>
+    <p>There are currently {{ countLinks }} links</p>
+
+    <button @click="removeAllLinks">Remove all links</button>
+    <p>{{ msg }}</p>
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapActions, mapMutations } from 'vuex';
+export default {
+  name: 'Stats',
+  data() {
+    return {
+      msg: ''
+    }
+  },
+  computed: {
+    ...mapGetters([
+      'countLinks'
+    ])
+  },
+  methods: {
+    ...mapMutations(['REMOVE_ALL']),
+    ...mapActions(['removeAll']),
+    removeAllLinks() {
+      this.removeAll().then(() => {
+        this.msg = 'They have been removed'
+      })
+    }
+  }
+}
+</script>
--- src/store.js
+++ src/store.js
@@ -5,12 +5,34 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-
+    title: 'Darwin Do',
+    links: ['http://google.com', 'http://youtube.com', 'http://coursera.com'],
   },
   mutations: {
-
+    ADD_LINK: (state, link) => {
+      state.links.push(link);
+    },
+    REMOVE_LINK: (state, link) => {
+      state.links.splice(link, 1);
+    },
+    REMOVE_ALL: (state) => {
+      state.links = [];
+    },
   },
   actions: {
-
+    removeLink: ({ commit }, link) => {
+      commit('REMOVE_LINK', link);
+    },
+    removeAll: ({ commit }) =>
+      new Promise((resolve, reject) => {
+        setTimeout(() => {
+          commit('REMOVE_ALL');
+          resolve();
+        }, 1500);
+      }),
+  },
+  getters: {
+    countLinks: state => state.links.length,
   },
 });
+
